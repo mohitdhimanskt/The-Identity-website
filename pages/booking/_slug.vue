7f2260a2bf5965c3d623af40c6710d7eb1f946ab
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-12-08 14:06:38 +0530

    Add to slug page

---
+++ pages/booking/_slug.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="uk-section">
+      <div class="uk-container uk-container-large">
+        <h1>{{ vechicle.title }}</h1>
+      </div>
+    </div>
+    <p>{{ vechicle.description}}</p>
+    <div class="phone">
+      {{ vechicle.price }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { getMetaTags } from "../../utils/seo";
+import { getStrapiMedia } from "../../utils/medias";
+
+export default {
+ async asyncData({ $strapi, params }) {
+    const matchingVechicle = await $strapi.find("vehicles", {
+      slug: params.slug
+    });
+    return {
+      vechicle: matchingVechicle[0]
+      // global: await $strapi.find("global"),
+    };
+  },
+  // head() {
+  //   const { defaultSeo, favicon, siteName } = this.global;
+
+  //   // Merge default and article-specific SEO data
+  //   const fullSeo = {
+  //     ...defaultSeo,
+  //     metaTitle: `${this.page.title} page`,
+  //     metaDescription: `All articles about ${this.page.title}`,
+  //   };
+
+  //   return {
+  //     title: fullSeo.metaTitle,
+  //     titleTemplate: `%s | ${siteName}`,
+  //     meta: getMetaTags(fullSeo),
+  //     link: [
+  //       {
+  //         rel: "favicon",
+  //         href: getStrapiMedia(favicon.url),
+  //       },
+  //     ],
+  //   };
+  // },
+};
+</script>
--- pages/vechicle/_slug.vue
+++ pages/vechicle/_slug.vue
@@ -1,27 +1,59 @@
 <template>
-  <div>
-    <div
-      v-if="vechicle.image"
+  <div class="column">
+    <div>
+      <div class="head">
+        <h1>{{ vechicle.title }}</h1>
+      </div>
+      <!-- <div
+      v-if="vechicle.images"
       id="banner"
       class="uk-height-small uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-padding"
-      :data-src="getStrapiMedia(vechicle.image.url)"
+      :data-src="getStrapiMedia(vechicle.images.url)"
       uk-img
     >
-      <h1>{{ vechicle.title }}</h1>
-    </div>
-
-    <div class="uk-section">
-      <div class="uk-container uk-container-small">
-        <!-- eslint-disable vue/no-v-html -->
-        <div
-          v-if="vechicle.content"
-          id="editor"
-          v-html="$md.render(vechicle.content)"
-        />
-        <!-- eslint-enable vue/no-v-html -->
-        <p v-if="vechicle.published_at">
-          {{ moment(vechicle.published_at).format("MMM Do YY") }}
+      
+    </div> -->
+      <div class="descript">
+        <p>
+          {{ vechicle.descriptions }}
         </p>
+      </div>
+
+      <div class="booking">
+        <nuxt-link
+          :to="{ name: 'booking-slug', params: { slug: vechicle.slug } }"
+          tag="a"
+        >
+          Book Now
+        </nuxt-link>
+      </div>
+
+      <div class="relate">
+        <h2>{{ vechicle.relative }}</h2>
+      </div>
+      <div class="gallery_container-all">
+        <div
+          class="gallery_container"
+          v-for="image in vechicle.images"
+          :key="image.id"
+        >
+          <img :src="getStrapiMedia(image.url)" height="40" />
+        </div>
+        <!-- {{vechicle.images}}  -->
+      </div>
+      <div class="uk-section">
+        <div class="uk-container uk-container-small">
+          <!-- eslint-disable vue/no-v-html -->
+          <div
+            v-if="vechicle.content"
+            id="editor"
+            v-html="$md.render(vechicle.content)"
+          />
+          <!-- eslint-enable vue/no-v-html -->
+          <!-- <p v-if="vechicle.published_at">
+          {{ moment(vechicle.published_at).format("MMM Do YY") }}
+        </p> -->
+        </div>
       </div>
     </div>
   </div>
@@ -33,24 +65,30 @@ import { getStrapiMedia } from "../../utils/medias";
 import { getMetaTags } from "../../utils/seo";
 
 export default {
+  props: {
+    buttons: {
+      type: Array
+    }
+  },
+
   async asyncData({ $strapi, params }) {
     const matchingVechicle = await $strapi.find("vehicles", {
-      slug: params.slug,
+      slug: params.slug
     });
     return {
-      vechicle: matchingVechicle[0],
+      vechicle: matchingVechicle[0]
       // global: await $strapi.find("global"),
     };
   },
   data() {
     return {
-      apiUrl: process.env.strapiBaseUri,
+      apiUrl: process.env.strapiBaseUri
     };
   },
   methods: {
     moment,
-    getStrapiMedia,
-  },
+    getStrapiMedia
+  }
   // head() {
   //   const { defaultSeo, favicon, siteName } = this.global;
 
@@ -76,3 +114,129 @@ export default {
   // },
 };
 </script>
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+html {
+  font: normal 20px montserrat;
+  overflow-x: hidden;
+}
+
+/* Gallery Start */
+.intro {
+  margin-top: 70px;
+  text-align: center;
+  margin-bottom: 40px;
+}
+
+.gallery_container-all {
+  width: 95%;
+  margin: 20px auto;
+  height: auto;
+  margin-bottom: 40px;
+}
+
+.gallery_container {
+  width: calc(33% - 6px);
+  overflow: hidden;
+  height: fit-content;
+  margin: 3px;
+  padding: 0;
+  display: inline-block;
+  position: relative;
+  float: none;
+  border-radius: 5px;
+  background-color: black;
+}
+
+img {
+  width: 100%;
+  transition-duration: 0.3s;
+  max-width: 100%;
+  display: block;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.gallery_title {
+  position: absolute;
+  display: block;
+  cursor: pointer;
+  top: 35%;
+  display: none;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  font-weight: bold;
+  font-size: 1.6em;
+  transition-duration: 0.3s;
+}
+
+.gallery_text {
+  position: absolute;
+  top: 70%;
+  cursor: pointer;
+  max-width: 80%;
+  text-align: center;
+  left: 50%;
+  font-size: 1em;
+  display: none;
+  margin-right: -50%;
+  transition-duration: 0.3s;
+  transform: translate(-50%, -50%);
+}
+
+.gallery_container:hover img {
+  transform: scale(1.2);
+  transition-duration: 0.3s;
+  opacity: 0.5;
+}
+
+.gallery_container:hover span {
+  color: white;
+  display: block;
+  transition-duration: 0.3s;
+}
+
+@media only screen and (max-width: 800px) {
+  .intro {
+    margin-top: 40px;
+  }
+  .gallery_container {
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+.relate {
+  text-align: center;
+  font-style: italic;
+  color: yellow;
+}
+.head {
+  text-align: center;
+  font-style: italic;
+  padding: 35px;
+}
+.descript {
+  text-align: left;
+  font-family: sans-serif;
+  font-size: 15pt;
+  color: #444;
+  width: 85%;
+
+  margin: 0 auto;
+  font-style: italic;
+
+  padding: 35px;
+}
+.booking {
+  text-align: center;
+  font-style: italic;
+  font-size: 20px;
+    
+}
+</style>
